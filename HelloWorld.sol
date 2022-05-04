861e16f35dea8383445b2d7e78639d5586b5d843
RiccardoGalbusera <riccardo@fiveelementslabs.com>
2022-05-04 15:12:18 +0200

    write comments in helloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,12 +1,17 @@
 pragma solidity 0.8.0;
 
+///@notice contract to store and retrieve a uint256 number
 contract HelloWorld {
     uint256 number;
 
+    ///@notice stores a number
+    ///@param _num the number to store
     function storeNumber(uint256 _num) public {
         number = _num;
     }
 
+    ///@notice returns the stored number
+    ///@return uint256 the stored number
     function retrieveNumber() public view returns (uint256) {
         return number;
     }
